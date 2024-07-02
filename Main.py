a666048ffed93153f8adc3cdfd43a397b81f7ea2
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-02 16:23:21 +0700

    Menambahkan beragam jenis pola segitiga, menambahkan list atau daftar pola segitiga yang tersedia untuk user bisa memilih dengan memasukkan nomor, membuat function proses setelah user memasukkan input dan di cetak langsung di function jenis pola segitiga yang dipilih

--- Main.py
+++ Main.py
@@ -7,19 +7,23 @@
 - Inverted right triangle = Segitiga siku siku terbalik
 '''
 
+def show_triangle_list():
+  for index, show_option in enumerate(triangle, start=1):
+    print(f'{index}. {show_option[1]}');
 
+# Jenis-jenis bentuk segitiga
 def right_triangle(limit):
   for i in range(1, limit + 1):
-    print(f'{s}' * i);
-  return "Segitiga siku - siku"
+    # proses print langsung ditunjukkan kesini
+    print(f'{star}' * i); #todo: harus diubah caranya dengan sekali perintah print daripada di cetak di function langsung
 
 def inverted_right_triangle(limit):
   for i in range(limit, 0, -1):
-    print('*' * i);
+    print(f'{star}' * i);
 
-def equilateral_triangle():
+def equilateral_triangle(limit):
   for i in range(limit):
-    print(' ' * (limit - i - 1 + '*' * (2 * i + 1)));
+    print(' ' * (limit - i - 1 + f'{star}' * (2 * i + 1)));
 
 def pascal_triangle(limit):
   for line in range(1, limit + 1):
@@ -28,27 +32,41 @@ def pascal_triangle(limit):
       print(C, end='');
       C = C * (line * i) // i;
     print();
+# ============================================================
 
-def show_triangle_list(triangle):
-  for index, show_option in enumerate(triangle, start=1):
-    print(f'{index}. {show_option}\n');
+#! proses mengambil hasil proses dari function jenis triangular-star-pattern belum berfungsi di function ini
+#todo: seharusnya ini juga menjadi function mengambil hasil dari function jenis triangular-star-pattern
+def process_the_limit_input(limit, triangle_option): # Menyimpan 2 parameter untuk di proses ke jenis segitiga yang dipilih
+  if triangle_option == triangle[0]:
+    hasil = right_triangle(limit);
+    return hasil;
+  elif triangle_option == triangle[1]:
+    hasil = inverted_right_triangle(limit);
+    return hasil;
+  elif triangle_option == triangle[2]:
+    hasil = equilateral_triangle(limit);
+    return hasil;
+  elif triangle_option == triangle[3]:
+    hasil = pascal_triangle(limit);
+    return hasil;
 
-def process_the_limit_input(limit):
-  if limit == triangle[0]:
-    return right_triangle;
-  elif limit == triangle[1]:
-    return inverted_right_triangle;
-  elif limit == triangle[2]:
-    return equilateral_triangle;
-  elif limit == triangle[3]:
-    return pascal_triangle;
 
-triangle = [right_triangle, inverted_right_triangle, equilateral_triangle, pascal_triangle];
-s = '*';
+
+triangle = [
+  [right_triangle, 'Segitiga siku-siku'],
+  [inverted_right_triangle, 'Segitiga siku-siku terbalik'],
+  [equilateral_triangle, 'Segitiga sama sisi'],
+  [pascal_triangle, 'Segitiga pascal']
+];
+star = '*';
+
+# User input
+show_triangle_list();
+triangle_option = int(input('Pilih opsi segitiga yang kamu inginkan!\n-> '));
+triangle_option = triangle_option - 1;
+print (f'kamu memilih: {triangle[triangle_option][1]}');
+
 
 limit = int(input('Masukkan angka: -> '));
-limit = limit - 1;
-process_the_limit_input(limit);
 
-result = process_the_limit_input(limit);
-print (f"Hasilnya adalah : {result}");
+final_result = process_the_limit_input(limit, triangle[triangle_option]);
