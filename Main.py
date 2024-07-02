cb91cb4b2ce50294721bf0980761e8ef16e8a722
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-02 19:04:15 +0700

    Menambahkan validasi input pilihan function pola segitiga, merapihkan kode, danr p memperbaiki error pada function pola segitiga sama sisi/equilateral triangle

--- Main.py
+++ Main.py
@@ -1,21 +1,30 @@
 
 ## Triangular star pattern with Python
 '''
-- Equilateral triangle = Segitiga sama sisi
-- Isosceles triangle = Segitiga sama kaki
 - Right triangle = Segitiga siku siku
 - Inverted right triangle = Segitiga siku siku terbalik
+- Equilateral triangle = Segitiga sama sisi
+- Pascal's triangle = Segitiga pascal
 '''
+
+def validateInput():
+  n = len(triangle);
+  index = triangle_option;
+  if 0 <= index <= n:
+    # Jika pilihan sesuai yang tertera, maka kembalikan nilai
+    return;
+  else:
+    raise ValueError(f'Masukkan pilihan sesuai nomor yang tertera dari 1 - {n}. Coba lagi!');
 
 def show_triangle_list():
   for index, show_option in enumerate(triangle, start=1):
     print(f'{index}. {show_option[1]}');
+# ==========================================================================================
 
 # Jenis-jenis bentuk segitiga
 def right_triangle(limit):
   for i in range(1, limit + 1):
-    # proses print langsung ditunjukkan kesini
-    print(f'{star}' * i); #todo: harus diubah caranya dengan sekali perintah print daripada di cetak di function langsung
+    print(f'{star}' * i);
 
 def inverted_right_triangle(limit):
   for i in range(limit, 0, -1):
@@ -23,34 +32,25 @@ def inverted_right_triangle(limit):
 
 def equilateral_triangle(limit):
   for i in range(limit):
-    print(' ' * (limit - i - 1 + f'{star}' * (2 * i + 1)));
+    print(' ' * (limit - i - 1), end = '');
+    print(star * (2 * i + 1));
 
 def pascal_triangle(limit):
   for line in range(1, limit + 1):
     C = 1; # Nilai pertama pada setiap baris selalu 1
     for i in range(1, line + 1):
-      print(C, end='');
-      C = C * (line * i) // i;
+      print(C, end=' ');
+      C = C * (line - i) // i;
     print();
 # ============================================================
 
-#! proses mengambil hasil proses dari function jenis triangular-star-pattern belum berfungsi di function ini
-#todo: seharusnya ini juga menjadi function mengambil hasil dari function jenis triangular-star-pattern
 def process_the_limit_input(limit, triangle_option): # Menyimpan 2 parameter untuk di proses ke jenis segitiga yang dipilih
-  if triangle_option == triangle[0]:
-    hasil = right_triangle(limit);
-    return hasil;
-  elif triangle_option == triangle[1]:
-    hasil = inverted_right_triangle(limit);
-    return hasil;
-  elif triangle_option == triangle[2]:
-    hasil = equilateral_triangle(limit);
-    return hasil;
-  elif triangle_option == triangle[3]:
-    hasil = pascal_triangle(limit);
-    return hasil;
+  if triangle_option == triangle[0]:right_triangle(limit);
+  elif triangle_option == triangle[1]:inverted_right_triangle(limit);
+  elif triangle_option == triangle[2]:equilateral_triangle(limit);
+  elif triangle_option == triangle[3]:pascal_triangle(limit);
 
-
+# ============================================================================================
 
 triangle = [
   [right_triangle, 'Segitiga siku-siku'],
@@ -62,11 +62,14 @@ star = '*';
 
 # User input
 show_triangle_list();
-triangle_option = int(input('Pilih opsi segitiga yang kamu inginkan!\n-> '));
+triangle_option = int(input('\n==================\n\nPilih opsi segitiga yang kamu inginkan!\n-> '));
+validateInput(); #! Validate input
+
 triangle_option = triangle_option - 1;
-print (f'kamu memilih: {triangle[triangle_option][1]}');
+print (f'kamu memilih: {triangle[triangle_option][1]}\n');
 
+limit = int(input('Masukkan ukuran dengan angka berapapun: -> '));
+print (f'Ukuran: {limit} x {limit}\n==============\n');
 
-limit = int(input('Masukkan angka: -> '));
-
-final_result = process_the_limit_input(limit, triangle[triangle_option]);
+# Output
+process_the_limit_input(limit, triangle[triangle_option]);
